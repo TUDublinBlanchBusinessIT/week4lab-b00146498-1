5029f1b01ffca0004b1b0c38db805adf4cca2847
B00146498 Rebecca Mazilu <B00146498@mytudublin.ie>
2023-10-11 10:10:02 +0100

    added part2.php

---
+++ part2.php
@@ -0,0 +1,27 @@
+<?php
+//Give the name of the program here
+//Rebecca mazilu 11/10/2023
+//This program creates a database for the client 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "tennisclub";
+$port = 3307;
+
+//set the default timezone - this is necessary since MySQL 8. This is an effort to store all dates and times together with their timezones. 
+//This is particularly important where there is a timestamp indicating when something happened.
+date_default_timezone_set('Europe/Dublin');
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname, $port);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO member (firstname, surname) VALUES ('john', 'doe')";
+
+mysqli_query($conn, $sql);
+
+mysqli_close($conn);
+?>
